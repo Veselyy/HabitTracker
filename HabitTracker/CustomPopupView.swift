118f499270a2bf08d6070dcb356c61cc25ba79c9
martinvesely <imthemartinveselyap@gmail.com>
2024-09-28 22:32:07 +0200

    refactor code

---
+++ HabitTracker/CustomPopupView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPopupView.swift
+//  HabitTracker
+//
+//  Created by Martin Veselý on 27.09.2024.
+//
+
+import SwiftUI
+
+struct CustomPopupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomPopupView()
+}
